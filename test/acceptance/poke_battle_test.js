f0be68dc431de165ffdba600b61d0b69ccd05ee3
localadmin <localadmin@compozed-tss-09.mobile.asu.edu>
2016-07-12 14:22:15 -0700

    removed comments from test file

--- test/acceptance/poke_battle_test.js
+++ test/acceptance/poke_battle_test.js
@@ -21,23 +21,23 @@ after(function(){
 describe('Pokemon Battle', function(){
   describe('#Main page', function(){
 
-    // it('should show a title', function(){
-    //   browser.get('/pokemon_battle')
-    //   element(by.tagName('h1')).getText().then(function(text){
-    //     expect(text).to.equal('Welcome to Pokemon Battle!')
-    //   })
-    // })
-    // it('User can see 9 pokemon to choose from', function(){
-    //   browser.get('/pokemon_battle')
-    //   browser.driver.sleep(10000)
-    //   element.all(by.tagName('li')).then(function(elements){
-    //     expect(elements.length).to.equal(9)
-    //   })
-    // })
-    // it('User can choose a pokemon and enter battle', function (){
-    //   rowser.get('/pokemon_battle')
-    //   browser.driver.sleep(10000)
-    // })
+    it('should show a title', function(){
+      browser.get('/pokemon_battle')
+      element(by.tagName('h1')).getText().then(function(text){
+        expect(text).to.equal('Welcome to Pokemon Battle!')
+      })
+    })
+    it('User can see 9 pokemon to choose from', function(){
+      browser.get('/pokemon_battle')
+      browser.driver.sleep(10000)
+      element.all(by.tagName('li')).then(function(elements){
+        expect(elements.length).to.equal(9)
+      })
+    })
+    it('User can choose a pokemon and enter battle', function (){
+      rowser.get('/pokemon_battle')
+      browser.driver.sleep(10000)
+    })
   })
 
   describe('#Battle page', function () {
