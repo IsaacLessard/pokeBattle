2d1a9c5310724ba5b0aaacb24e86a0d2bf6ed256
localadmin <localadmin@compozed-tss-09.mobile.asu.edu>
2016-07-12 14:16:37 -0700

    :truck: added battle-view

--- routes/pokemon_battle.js
+++ routes/pokemon_battle.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('pokemon_battle/home');
 });
 
+router.get('/battle', function(req, res) {
+  res.render('pokemon_battle/battle')
+})
+
 module.exports = router;
--- test/acceptance/poke_battle_test.js
+++ test/acceptance/poke_battle_test.js
@@ -21,22 +21,53 @@ after(function(){
 describe('Pokemon Battle', function(){
   describe('#Main page', function(){
 
+    // it('should show a title', function(){
+    //   browser.get('/pokemon_battle')
+    //   element(by.tagName('h1')).getText().then(function(text){
+    //     expect(text).to.equal('Welcome to Pokemon Battle!')
+    //   })
+    // })
+    // it('User can see 9 pokemon to choose from', function(){
+    //   browser.get('/pokemon_battle')
+    //   browser.driver.sleep(10000)
+    //   element.all(by.tagName('li')).then(function(elements){
+    //     expect(elements.length).to.equal(9)
+    //   })
+    // })
+    // it('User can choose a pokemon and enter battle', function (){
+    //   rowser.get('/pokemon_battle')
+    //   browser.driver.sleep(10000)
+    // })
+  })
+
+  describe('#Battle page', function () {
     it('should show a title', function(){
-      browser.get('/pokemon_battle')
+      browser.get('/pokemon_battle/battle');
       element(by.tagName('h1')).getText().then(function(text){
-        expect(text).to.equal('Welcome to Pokemon Battle!')
+        expect(text).to.equal('Poke Battle!');
+      });
+    });
+
+    it('should have profile name and picctures of the 2 pokemons', function(){
+      browser.get('/pokemon_battle/battle');
+      element.all(by.tagName('h3')).then(function(elements){
+        expect(elements.length).to.equal(2)
       })
-    })
-    it('User can see 9 pokemon to choose from', function(){
-      browser.get('/pokemon_battle')
-      browser.driver.sleep(10000)
-      element.all(by.tagName('li')).then(function(elements){
-        expect(elements.length).to.equal(9)
+      element.all(by.tagName('img')).then(function(elements){
+        expect(elements.length).to.equal(2)
       })
-    })
-    it('User can choose a pokemon and enter battle', function (){
-      rowser.get('/pokemon_battle')
-      browser.driver.sleep(10000)
+    });
+    it('should show the data of the selected pokemon', function() {
+      browser.get('/pokemon_battle/battle')
+      element.all(by.tagName('h3')).getText().then(function(elements){
+        expect(elements).to.deep.equal(['ekans', 'bulbasaur'])
+      })
+      element.all(by.tagName('img')).getAttribute('src').then(function(elements){
+        expect(elements).to.deep.equal(['http://pokeapi.co/media/sprites/pokemon/23.png', 'http://pokeapi.co/media/sprites/pokemon/1.png'])
+      })
+      element.all(by.tagName('ul')).then(function(elements) {
+        expect(elements.length).to.equal(2)
+      })
     })
   })
 })
