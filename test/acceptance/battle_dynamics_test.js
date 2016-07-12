be57353f120f42186327de88f8f5fa6f814b360b
localadmin <localadmin@compozed-tss-09.mobile.asu.edu>
2016-07-12 14:50:50 -0700

    added the buttons to make your ballsy moves

---
+++ test/acceptance/battle_dynamics_test.js
@@ -0,0 +1,25 @@
+require('../helper');
+
+var http = require('http'),
+    server;
+
+before(function() {
+  server = http.createServer(require('../../app'));
+  server.listen(0);
+  browser.baseUrl = 'http://localhost:' + server.address().port;
+});
+
+beforeEach(function() {
+  return browser.ignoreSynchronization = true;
+});
+
+after(function(){
+  server.close();
+});
+
+describe('battle dynamics', function(){
+	it('when a player clicks on a move, HP updates', function(){
+    browser.get('/pokemon_battle/battle');
+    
+	})
+})
